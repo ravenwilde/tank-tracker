307aeec1c406dc62dd335e869d2f4e7dc4bb3e27
ravenwilde <jennifer@ravenwilde.com>
2014-01-02 23:20:19 -0500

    did not really change much

--- tank-tracker.php
+++ tank-tracker.php
@@ -70,19 +70,10 @@ function create_journal_post_types() {
     );
 }
 
+/* setup & call water parameter metaboxes */
 include_once 'metaboxes/setup.php';
-
 include_once 'metaboxes/water-params-spec.php';
 
-//include_once 'metaboxes/simple-spec.php';
- 
-//include_once 'metaboxes/full-spec.php';
-
-// include_once 'metaboxes/checkbox-spec.php';
-
-// include_once 'metaboxes/radio-spec.php';
-
-// include_once 'metaboxes/select-spec.php';
 
 
 /* Make everything happen */
